964ee5ba4be7216f62592b47d46faae1b0f601d5
Mizan <mizab19@gmail.com>
2021-08-23 21:33:22 +0100

    Update fileHandler.py

--- scripts/fileHandler.py
+++ scripts/fileHandler.py
@@ -8,25 +8,25 @@ class FileHandler:
     def to_csv(self, df, csvPath, index=False):
         try:
             df.to_csv(csvPath, index=index)
-            self.logger.info(f'Csv file saved in {csvPath}.')
+            print('file saved')
 
         except Exception:
-            self.logger.exception('File saving failed.')
+            print('file saving failed')
 
-    def read_csv(self, csvPath, missing_values=["n/a", "na", "undefined"]):
+    def readCsv(self, csvPath, missing_values=["undefined","na"]):
         try:
-            df = pd.read_csv(csvPath, na_values=missing_values)
-            self.logger.info(f'Csv file read from {csvPath}.')
+            df = pd.readCsv(csvPath, na_values=missing_values)
+            print('csv file read from {csvPath}.')
             return df
 
         except FileNotFoundError:
-            self.logger.exception('File not found.')
+            print('file not found.')
 
-    def read_json(self, json_path):
+    def readJson(self, json_path):
         try:
             with open(json_path) as js:
                 json_obj = json.load(js)
-            self.logger.info(f'Json file read from {json_path}.')
+            print('json file read from {json_path}.')
             return json_obj
 
         except FileNotFoundError:
